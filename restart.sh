0432e723efa5b96ca4a316ccad6d9d7010041395
Fiaguhop137 <eastonzou@gmail.com>
2026-08-19 21:07:35 -0400

    Auto-commit: 2026.08.19.21.07.35

--- restart.sh
+++ restart.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+sleep $((RANDOM % 10 + 1))
+kill $(pgrep -f "restart.sh" | grep -v "^$$$") 2>/dev/null
 cd "$(dirname "$0")"
-sleep 1
 git pull
 git push
+pkill -f bot.py
 nohup python3 bot.py > bot_runtime.log 2>&1 &
+
