36791c5fbf82ca69657eecc3ab7b72b33ce0904e
Fiaguhop137 <eastonzou@gmail.com>
2026-09-07 22:04:56 -0400

    Auto-commit: 2026.09.07.22.04.56

--- restart.sh
+++ restart.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-sleep $((RANDOM % 10 + 1))
-kill $(pgrep -f "restart.sh" | grep -v "^$$$") 2>/dev/null
-cd "$(dirname "$0")"
-git pull
-git push
-pkill -f bot.py
-nohup python3 bot.py > bot_runtime.log 2>&1 &
 
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
+export HOME=/home/firebot
+export USER=firebot
+export LOGNAME=firebot
+export XDG_RUNTIME_DIR=/run/user/1000
+
+sleep $((RANDOM % 10 + 1))
+
+kill $(/usr/bin/pgrep -f "restart.sh" | /usr/bin/grep -v "^$$$") 2>/dev/null || :
+
+cd /home/firebot/git/Emberbot137
+
+/usr/bin/git pull
+/usr/bin/git push
+
+/usr/bin/pkill -f "bot.py" || :
+
+/usr/bin/nohup /usr/bin/python3 bot.py > bot_runtime.log 2>&1 &
